5cea300346764fa74c5bb67832808e7307a7896d
Aleksei Neronov <pribambasa@mail.ru>
2016-06-23 15:02:17 +1000

    Create Running Time of Algorithms.swift

---
+++ Running Time of Algorithms.swift
@@ -0,0 +1,14 @@
+import Foundation
+let n = Int(readLine()!)!
+var arr = String(readLine()!).characters.split{$0 == " "}.map(String.init).map { Int($0)!}
+var count = 0
+for i in 1..<n{
+    var l = i
+    while arr[l-1] > arr[l] {
+        swap(&arr[l], &arr[l-1])
+        l -= 1
+        count += 1
+        if l<1 { break }
+    }
+}
+print(count)

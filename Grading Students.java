35e4a82fd4ea5dcc21a716dd3e07ede7ea0fef35
Sergei Neronov <neronovs@mail.ru>
2017-12-14 09:38:50 +0900

    Create Grading Students.java

---
+++ Grading Students.java
@@ -0,0 +1,34 @@
+public class Solution {
+    
+    static int[] solve(int[] grades){
+        // Complete this function
+        for (int i = 0; i < grades.length; i++) {
+            int grade = grades[i];
+            int nextMult5 = grade;
+            while (nextMult5 % 5 != 0) {
+                nextMult5++;
+            }
+            if (grade >= 38) {
+                if (nextMult5 - grade < 3)
+                    grades[i] = nextMult5;
+                else if (nextMult5 - grade == 3)
+                    grades[i] = grade;
+            }
+        }
+        return grades;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] grades = new int[n];
+        for(int grades_i=0; grades_i < n; grades_i++){
+            grades[grades_i] = in.nextInt();
+        }
+        int[] result = solve(grades);
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + (i != result.length - 1 ? "\n" : ""));
+        }
+        System.out.println("");
+    }
+}
